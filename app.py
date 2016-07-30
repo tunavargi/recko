c53ab2f4730b40b126bc7c9079dbdd4886f5595d
vargi <tunavargi@gmail.com>
2016-07-30 12:46:42 +0300

    inital commit

---
+++ app.py
@@ -0,0 +1,55 @@
+import json
+from datetime import datetime
+from random import randint
+
+import redis
+import requests
+from bson import ObjectId
+from bson.json_util import dumps
+
+from flask import Flask
+from flask import request
+from config import EMBEDLY_API_KEY, REDIS_HOST, DB_NAME
+from pymongo import MongoClient
+
+
+app = Flask(__name__)
+client = MongoClient()
+redisconn = redis.StrictRedis(host=REDIS_HOST, port=6379, db=0)
+db = client[DB_NAME]
+
+
+def get_keywords(url):
+    req_url = "https://api.embedly.com/1/extract"
+    response = requests.get(req_url, params={"url": url,
+                                         "key": EMBEDLY_API_KEY})
+    result = response.json()
+    return result.get("keywords")
+
+
+@app.route('/teach', methods=['GET'])
+def teach():
+    data = request.args
+    url = data.get("url")
+    keywords = get_keywords(url)
+    if not db.articles.find_one({"url": url}):
+        item = db.articles.insert_one({"url": url,
+                                     "create_date": datetime.now(),
+                                     "keywords": keywords})
+        redisconn.rpush("queue", str(item.inserted_id))
+    return 'thanks'
+
+
+@app.route('/neighbors/<string:id>', methods=["GET"])
+def neighbors(id):
+    query = {"$or":[{"match1": ObjectId(id)}, {"match2": ObjectId(id)}]}
+    similar = db.article_match.find(query).sort([("dst", -1)])
+    match_ids = [i["match1"] if i["match1"] == ObjectId(id) else i["match2"] for i in similar]
+    if match_ids:
+        match_id = match_ids[randint(0, len(match_ids)-1)]
+        article = db.articles.find_one({"_id": ObjectId(match_id)})
+        return dumps({"url": article["url"]})
+    return json.dumps({})
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ calculate.py
@@ -0,0 +1,41 @@
+import redis
+from bson import ObjectId
+from config import REDIS_HOST, DB_NAME
+from pymongo import MongoClient
+from scipy.spatial import distance
+
+client = MongoClient()
+redisconn = redis.StrictRedis(host=REDIS_HOST, port=6379, db=0)
+db = client[DB_NAME]
+
+
+def calculate_distance(new_article, matched_article):
+    "Calculate the distance between matched url and recored url"
+
+    new_article_kws = {i["name"]: i["score"] for i in new_article["keywords"] if i['score'] >= 50}
+    matched_article_kws = {i["name"]: i["score"] for i in matched_article["keywords"] if i['score'] >= 50}
+    all_keywords = [i for i in [i["name"] for i in new_article["keywords"] if i["score"] >= 50]] + [i for i in [i["name"] for i in matched_article["keywords"] if i["score"] >= 50]]
+    a = tuple([new_article_kws.get(i, 0) for i in all_keywords])
+    b = tuple([matched_article_kws.get(i, 0) for i in all_keywords])
+    dst = distance.euclidean(a,b)
+
+    return dst
+
+
+def calculater():
+    "Article Distance Calculating worker"
+
+    while True:
+        id = redisconn.blpop('queue')
+        article = db.articles.find_one({"_id": ObjectId(id[1])})
+        words = [i["name"] for i in article["keywords"] if i['score'] > 45]
+        matching_urls = db.articles.find({"keywords.name": {"$in": words}, "_id": {"$ne": ObjectId(id[1])}})
+        for i in matching_urls:
+            dst = calculate_distance(article, i)
+            db.article_match.insert_one({"match1": article['_id'],
+                                         "match2": i['_id'],
+                                         "dst": dst})
+
+
+if __name__ == "__main__":
+    calculater()
