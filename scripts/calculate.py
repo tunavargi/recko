dc4fbca164c27d3401c21a0cab54d76a99a6f84f
vargi <tunavargi@gmail.com>
2016-08-18 20:58:58 +0300

    calculate script fix

--- scripts/calculate.py
+++ scripts/calculate.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
 from models.articles import Article, ArticleMatch
 from scipy.spatial import distance
 from config import REDIS_HOST
-
+from app import db
 redisconn = redis.StrictRedis(host=REDIS_HOST, port=6379, db=0)
 
 def calculate_euclidaen_distance(new_article, matched_article):
@@ -29,18 +29,28 @@ def calculate_article(article):
     words = [i['name'] for i in article.keywords if i.get("score", 0) > 45]
     matching_urls = Article.q.filter({"keywords.name": {"$in": words},
                                       "_id": {"$ne": article._id}}).all()
+    count = 0
+    bulk = db.test.initialize_ordered_bulk_op()
     for i in matching_urls:
-
+        print i.id
+        if count > 200:
+            bulk.execute()
+            bulk = db.test.initialize_ordered_bulk_op()
+            count = 0
         dst = calculate_euclidaen_distance(article, i)
-        article1 = ArticleMatch(**{"match1": article.id,
-                                   "match2": i.id,
-                                   "dst":dst})
-        article2 = ArticleMatch(**{"match2": article.id,
-                                   "match1": i.id,
-                                   "dst":dst})
-        article1.save()
-        article2.save()
-        print article.id
+        if not ArticleMatch.q.filter({"match1": article.id, "match2": i.id}).first():
+            bulk.insert(
+                {"match1": article.id,
+                 "match2": i.id,
+                 "dst": dst}
+            );
+            bulk.insert(
+                {"match2": article.id,
+                 "match1": i.id,
+                 "dst": dst}
+            );
+            print 'inserted->', i.id
+            count += 1
 
 
 def calculate_all():
@@ -63,4 +73,4 @@ def calculater():
 
 
 if __name__ == "__main__":
-    calculater()
+    calculate_all()
